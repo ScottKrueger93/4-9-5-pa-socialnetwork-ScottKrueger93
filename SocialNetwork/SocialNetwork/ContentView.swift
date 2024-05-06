91ff8dd04c92ad1d5f5a1ef3f3f7147220ef0604
scott <krueger-scott@gmx.de>
2024-05-06 12:45:00 +0200

    ???

---
+++ SocialNetwork/SocialNetwork/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SocialNetwork
+//
+//  Created by Scott Krügerq on 19.04.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SocialNetwork/SocialNetwork/SocialNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialNetworkApp.swift
+//  SocialNetwork
+//
+//  Created by Scott Krügerq on 19.04.24.
+//
+
+import SwiftUI
+
+@main
+struct SocialNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
